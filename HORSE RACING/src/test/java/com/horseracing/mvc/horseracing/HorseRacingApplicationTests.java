8f00329d86dd51cc20b5b4a0bc2c1bf3bbe1c301
hao-techsad <haovnase182622@fpt.edu.vn>
2026-07-03 14:56:48 +0700

    structure

---
+++ HORSE RACING/src/test/java/com/horseracing/mvc/horseracing/HorseRacingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horseracing.mvc.horseracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseRacingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
