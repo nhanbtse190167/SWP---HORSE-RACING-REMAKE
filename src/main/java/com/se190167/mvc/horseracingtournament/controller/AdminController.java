d5221bbb1a0c0f9740f589f4bf833da8bdd98eb4
Bùi Thành Nhân <nhanthach732@gmail.com>
2026-07-12 04:05:31 +0700

    Thêm giao diện HTML và hoàn thiện khung Controller, Entity

---
+++ src/main/java/com/se190167/mvc/horseracingtournament/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.se190167.mvc.horseracingtournament.controller;
+
+public class AdminController {
+}
---
+++ src/main/java/com/se190167/mvc/horseracingtournament/entity/Ranking.java
@@ -0,0 +1,4 @@
+package com.se190167.mvc.horseracingtournament.entity;
+
+public class Ranking {
+}
